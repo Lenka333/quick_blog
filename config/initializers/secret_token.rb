6cfb31658f11456de3451c6de28a7a02c71a2328
Lenka <lenkamailbox@gmail.com>
2015-01-27 19:44:20 +1100

    first test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = '27cd70e6f6d79e2307b76d58ff4ddcb8ede65c70c55d89c89eec845940b8b7efb43c4a520e4511e7d6a04a0cbbcc6b1e3e8d2e2ea668c29c3fa3625cb4efb372'
